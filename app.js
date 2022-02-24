a1e89e4f71fda7ae35a19ee3ab97ba855617c333
Emmanuel <pescuesera@gmail.com>
2022-02-24 12:54:12 -0600

    Ajustes de prueba

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// let title = 'Hola!'
+
+// document.getEleme  ntById('h1')
+// h1.innerText = title
+
+// title = 'Chau!'
+// h1.innerText = title
+
+// const app = 
+
+ new Vue ({
+    el: '#app',
+    
+     data () {
+       return{
+         title: 'Hola Vue!'
+       }
+     }
+    
+  })  
